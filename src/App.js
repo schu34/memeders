d985d8d3bc8ae1c08b7c0934baf46f2892c0ea23
Esmeralda R <esmeralda.re@outlook.com>
2018-07-12 19:36:51 -0400

    error unexpected token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+import logo                from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-     <div>hello</div>
-    )
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return <div>{this.props.children}</div>
+    }
 }
 
 export default App
