038b6537a6520eed1abf5bb31830761266d2d4b1
Nicky Kwan <nickykwn@gmail.com>
2018-07-13 02:45:30 -0400

    rectangle added with page detection

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,22 +5,28 @@ class Navbar extends Component {
 		super(props);
 	}
 
+	getPath(location) {
+
+    	const arr = location.split("/")
+    	return arr[arr.length-1];
+	}	
+
 	render () {
-		console.log("HERE", window.location)
+		console.log("HERE", window.location.pathname)
 		return(
 			<div>
 				<div className="navbar-container">
 					<a href="/swipe" className="navbar" alt="profile">
 						<img src="img/Profile.png" />
-						{window.location==="/home" ? <img src="img/Reactangle.png" /> : null}
+						{window.location.pathname ==="/swipe" ? <img src="img/Rectangle.png" className="rectangle" /> : null}
 					</a>
 					<a href="/matches" className="navbar" alt="match">
 						<img src="img/Match.png" />
-						{window.location==="/match" ? <img src="img/Reactangle.png" /> : null}
+						{window.location.pathname ==="/match" ? <img src="img/Rectangle.png" className="rectangle" /> : null}
 					</a>
 					<a href="/chat" className="navbar" alt="chat">
 						<img src="img/chat.png" />
-						{window.location==="/chat" ? <img src="img/Reactangle.png" /> : null}
+						{window.location.pathname ==="/chat" ? <img src="img/Rectangle.png" className="rectangle" /> : null}
 					</a>
 				</div>
 			</div>
