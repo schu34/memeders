10a1f9d8be562cdbd836466a767dacdab4b491d8
Esmeralda R <esmeralda.re@outlook.com>
2018-07-13 00:41:00 -0400

    swipe module

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,30 @@
 import React, { Component }                         from 'react';
+import Swipeable from 'react-swipeable'
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            memes: [],
+        }
 
-	render () {
-		return (
-			<div>
-				
-			</div>
-		)
-	}
+        this.onSwipedRight = this.onSwipedRight.bind(this);
+        this.onSwipedLeft  = this.onSwipedLeft.bind(this);
+    }
+
+    onSwipedRight(){}
+
+    onSwipedLeft(){}
+
+    render () {
+        return (
+            <Swipeable
+                preventDefaultTouchmoveEvent
+                onSwipedRight={this.onSwipedRight}
+                onSwipedLeft={this.onSwipedLeft} >
+            </Swipeable>
+        )
+    }
 }
 
 export default Home
